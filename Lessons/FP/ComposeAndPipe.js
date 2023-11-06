dc30f798eb82689443f853fd2ca5a397a0fd6f03
Riwalenn Bas <riwalenn@gmail.com>
2023-11-06 19:45:53 +0100

    [MAJ] add compose and pipe lesson

--- Lessons/FP/ComposeAndPipe.js
+++ Lessons/FP/ComposeAndPipe.js
@@ -1,8 +1,8 @@
 // Compose
-const compose = (f, g) => (data) => f(g(data));
+const compose = (f, g) => (data) => f(g(data)); // arity of two
 
-const multiplyBy3 = (num) => num * 3;
-const makePositive = (num) => Math.abs(num);
+const multiplyBy3 = (num) => num * 3; // arity of one
+const makePositive = (num) => Math.abs(num); // arity of one
 const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive);
 
 console.log(multiplyBy3AndAbsolute(-50));
@@ -11,4 +11,5 @@ console.log(multiplyBy3AndAbsolute(-50));
 
 // Pipe
 // fn1(fn2(fn3(50)))
-const pipe = (f, g) => (data) => g(f(data));
+const pipe = (f, g) => (data) => g(f(data)); // arity of two
+
