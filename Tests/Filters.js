e84914f75ca323c0bc8469aeaede94ba44a32343
Riwalenn Bas <riwalenn@gmail.com>
2023-11-08 07:27:29 +0100

    [MAJ] test filters & copy objects

--- Tests/Filters.js
+++ Tests/Filters.js
@@ -15,7 +15,13 @@ const liste = [
     { id: 10, name: 'pc 7', active: true, type: TYPE_PERSONNAL_COMPUTER },
 ];
 
-let results = [];
+let results1 = [];
+let results2 = [];
+let results3 = [];
+let results4 = [];
+let results5 = [];
+let results6 = [];
+
 const compose = (f, g) => (data) => f(g(data));
 
 const filterStatus = (data) => data.filter(d => d.active === true);
@@ -23,5 +29,30 @@ const filterType   = (data) => data.filter(d => d.type === TYPE_PERSONNAL_COMPUT
 
 const filteredList = compose(filterType, filterStatus);
 
-results = { ...filteredList(liste)};
-console.log(results);
+results1 = { ...filteredList(liste)};
+filteredList(liste).forEach((v) => results2.push(v));
+
+results3 = filteredList(liste);
+filteredList(liste).forEach((v) => results4 = { ...v})
+
+results5 = Object.assign({}, filteredList(liste))
+
+results6 = JSON.parse(JSON.stringify(filteredList(liste)));
+
+const test = { name: 'test', active: true, type: TYPE_PERSONNAL_COMPUTER };
+
+liste.push(test)
+
+// results1.push(test);
+results2.push(test);
+results3.push(test);
+// results4.push(test);
+// results5.push(test);
+results6.push(test);
+
+console.log("result 1", results1);
+console.log("result 2", results2);
+console.log("result 3", results3);
+console.log("result 4", results4);
+console.log("result 5", results5);
+console.log("result 6", results6);
