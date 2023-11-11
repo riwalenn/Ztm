40c5dd8967589b089676ce90bdf370b3607bfb44
Riwalenn Bas <riwalenn@gmail.com>
2023-11-11 13:31:16 +0100

    (ADD) data structure : implementing an array lesson

---
+++ Lessons/DataStructure/ImplementingAnArray.js
@@ -0,0 +1,50 @@
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+
+        return this.length;
+    }
+
+    pop() {
+        const lastItem = this.data[this.length-1];
+        delete this.data[this.length-1];
+        this.length--;
+
+        return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+
+        return item;
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i+1];
+        }
+        delete this.data[this.length-1];
+        this.length--;
+    }
+}
+
+const newArray = new MyArray();
+newArray.push('hi');
+newArray.push('you');
+newArray.push('!');
+newArray.push('are');
+newArray.push('nice');
+// newArray.pop();
+newArray.delete(2);
+console.log(newArray)
