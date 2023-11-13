fd296f218596da7deb417dfd72acf6dca418fdc1
Riwalenn Bas <riwalenn@gmail.com>
2023-11-13 21:33:23 +0100

    (AJ) deep merge exercice => array are not merge but assign

--- Tests/DeepMerge.js
+++ Tests/DeepMerge.js
@@ -5,6 +5,12 @@ let defaultParameter = {
         id: null
     },
     test: false,
+    name: "DEFAULT CLIENT NAME",
+    testString: "TEST STRING",
+    testEmptyString: "TEST EMPTY STRING",
+    testEmptyObject: { step: 0 },
+    testNull: { step: 0 },
+    testNull2: 0,
     access: {
         isForAssociation: false,
         isForEnterprise: false,
@@ -33,13 +39,16 @@ let defaultParameter = {
         module1: ['item1', 'item2','item3','item4','item5','item6','item7','item8'],
         module2: ['item1','item2','item3','item4','item5'],
         module3: ['item1','item2','item3'],
-    }
+    },
 }
 
 const paramClients = [
     {
         id: 2,
         userType: ["enterprise"],
+        name: "CLIENT ENTERPRISE NAME",
+        testString: "DO NOT TEST STRING",
+        testEmptyString: "",
         client: {
             id: 88965
         },
@@ -50,10 +59,15 @@ const paramClients = [
             module1: ['item1','item3','item5','item7'],
             module2: ['item1','item3','item5'],
             module3: ['item1','item3'],
-        }
+        },
     },
     {
         id: 3,
+        name: "CLIENT STRUCTURE NAME",
+        testEmptyString: "", // return ""
+        testEmptyObject: {}, // return {}
+        testNull: {step: null}, // return {step:null}
+        testNull2: null, // return null
         userType: ["structure"],
         client: {
             id: 88965
@@ -62,17 +76,18 @@ const paramClients = [
             isForStructure: true,
             isForEnterprise: true,
         },
+
         modules: {
             module1: ['item2','item4','item6','item8','item10','item12','item14','item16','item18','item20','item22'],
             module2: ['item2','item4'],
-            module3: [],
+            module3: [], // return []
         },
         orderFields: {
             field1: "order-3",
             field2: "order-2",
             field3: "order-1",
             field4: "order-4",
-        }
+        },
     },
     {
         id: 4,
@@ -134,7 +149,7 @@ function mergeDeep(target, source) {
 
     Object.keys(source).forEach((key) => {
         if (source[key] && typeof source[key] === 'object' && !isArray(source[key])) {
-            if (!target[key]) {
+            if (!target[key] || Object.values(source[key]).length === 0) {
                 Object.assign(target, { [key]: {} });
             }
             mergeDeep(target[key], source[key]);
