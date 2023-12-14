c444c0c00bc58df4c26bcbb5fbd26b1aa78f0b34
Riwalenn Bas <riwalenn@gmail.com>
2023-12-14 20:43:38 +0100

    (MAJ) add exercices and google interview Exercices

--- Exercices/LinkedList.js
+++ Exercices/LinkedList.js
@@ -17,9 +17,23 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    prepend(value) {
+        const newNode = {
+            value: value,
+            next: null
+        };
+
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
 }
 
+//1-->10-->16 add 1 with prepend
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.prepend(1);
 console.log(myLinkedList);
