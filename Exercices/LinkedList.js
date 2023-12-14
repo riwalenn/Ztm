1de371f1dbefc48d21a2be74b3fd705c030586a2
Riwalenn Bas <riwalenn@gmail.com>
2023-12-14 21:48:52 +0100

    (MAJ) add exercices and google interview Exercices

--- Exercices/LinkedList.js
+++ Exercices/LinkedList.js
@@ -29,11 +29,57 @@ class LinkedList {
         this.length++;
         return this;
     }
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+    insert(index, value) {
+        if(index === 0) {
+            return this.prepend(value);
+        }
+
+        if (index >= this.length) {
+            return this.append(value);
+        }
+
+        const newNode = {
+            value: value,
+            next: null
+        };
+
+        const leader = this.traverseToIndex(index-1);
+        const follower = leader.next;
+
+        leader.next = newNode;
+        newNode.next = follower;
+
+        this.length++;
+        return this;
+    }
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
 }
 
-//1-->10-->16 add 1 with prepend
+//1-->10-->5-->16 add 1 with prepend
+//1-->10-->99-->5-->16 add 99 at index 2
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+// myLinkedList.insert(0, 9);
+myLinkedList.insert(2, 99);
+// myLinkedList.insert(200, 999);
+console.log(myLinkedList.printList());
+// console.log(myLinkedList);
