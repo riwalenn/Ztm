071834cfd841e108cd2c8e027805deaf629037ac
Riwalenn Bas <riwalenn@gmail.com>
2023-12-22 16:12:00 +0100

    (MAJ) add exercices and google interview Exercices

--- Exercices/Search/BFSandDFS.js
+++ Exercices/Search/BFSandDFS.js
@@ -1,5 +1,9 @@
 //BFS: Shortest Path, Closer Nodes => More Memory than DFS
 //DFS: Less Memory, Does Path exist ? => Can get Slow
+//Dijkstra: shortest path - faster, more efficient with the downside that it can accommodate for negative weights between nodes
+//Bellman-Ford: very effective at solving the shortest path over Dijkstra algorithm: can accommodate negative weights
+//can take a long time to run in terms of complexity, not the most efficient algorithm
+//worst case for Bellman-Ford algorithm is usually time complexity of O(n²)
 
 //If you know a solution is not far from the root of the tree: BFS
 
